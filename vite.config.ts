e537b4c71bb94fa83d688215f3ef197311285617
hatem <hatemjerbi@MBP-de-hatem.lan>
2024-12-05 13:35:26 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { scriptEnvInjector } from "./scriptEnvInjectorPlugin";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), scriptEnvInjector()],
+  base: "/vitetr-landing-page/",
   server: {
     port: 4173
   }
